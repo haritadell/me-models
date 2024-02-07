4471a7c11b0334d931778af494328edc7d3df3f4
haritadell <h.dellaporta@gmail.com>
2024-02-07 21:58:48 +0000

    try with simulated data

--- mental_health_study/train.py
+++ mental_health_study/train.py
@@ -8,21 +8,23 @@ folder_path = './results/'
 
 def train_(params):
     B, m, c, T = params
-    data = process_csv('nhanes_data.csv')
-    npl_ = npl_class(data,int(B),int(m), c, T, seed, lx=10, ly=100, prior=1.0)
+    # data_control, data_treat = process_csv('mental_health_data.csv')
+    data_control, data_treat = process_csv('sim_data_bcr2.csv')
+    data = np.vstack((data_control, data_treat))
+    npl_ = npl_class(data,int(B),int(m), c, T, seed, lx=10, ly=100, prior=1)
     t0 = time.time()
     npl_.draw_samples()
     t1 = time.time()
     total = t1-t0
     sample = npl_.sample
     knots_sample = npl_.knots_sample
-    np.savetxt(folder_path+f'bootstrap_samples.txt', sample.reshape((int(B),64)))
-    np.savetxt(folder_path+f'bootstrap_samples_knots.txt', knots_sample.reshape((int(B),30)))
+    np.savetxt(folder_path+f'bootstrap_samples_bcr22.txt', sample.reshape((int(B),64)))
+    np.savetxt(folder_path+f'bootstrap_samples_knots_bcr22.txt', knots_sample.reshape((int(B),30)))
     return sample, knots_sample
       
 B = 200 
 m = 1
-c = 50
+c = 100
 T = 100
 
 params = np.array([B,m,c,T])
