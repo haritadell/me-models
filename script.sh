59da891129f32f3473c43efa0aa1d88351dfb110
haritadell <h.dellaporta@gmail.com>
2024-02-27 17:10:33 +0000

    example slurm script

---
+++ script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#SBATCH --job-name=classical-unif # Job name for tracking
+#SBATCH --partition=gecko  # Partition you wish to use (see above for list)
+#SBATCH --cpus-per-task=40     # Number of CPU threads used by your job, set this upto 40 as required.
+#SBATCH --mem=60000            # 60GB RAM
+#SBATCH --exclusive=mcs        # Exclusive mode, only this job will run
+#SBATCH --time=2-00:00:00      # Job time limit set to 2 days (48 hours)
+#
+#SBATCH --mail-type=END,FAIL,TIME_LIMIT_80 # Events to send email on, remove if you don't want this
+#SBATCH --output=joboutput_%j.out # Standard out from your job
+#SBATCH --error=joboutput_%j.err  # Standard error from your job
+
+## Execute your program(s) ##
+srun python /dcs/pg23/u1604520/me-models/train.py 12
