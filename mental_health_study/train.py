cdf63b533b8d2464cd674720535391f4fdb3138c
haritadell <h.dellaporta@gmail.com>
2024-03-27 11:26:04 +0000

    latest train function for mental health study

--- mental_health_study/train.py
+++ mental_health_study/train.py
@@ -2,32 +2,35 @@ import numpy as np
 from npl import npl_class
 import time
 from utils import process_csv
+from tqdm import tqdm 
 
-seed = 17
-folder_path = './results/'
+folder_path = './results/mh/bcr_sim_data2/'
 
-def train_(params):
+def train_(params, path_data, r):
     B, m, c, T = params
-    # data_control, data_treat = process_csv('mental_health_data.csv')
-    data_control, data_treat = process_csv('sim_data_bcr2.csv')
+    #data_control, data_treat = process_csv('./me-models/mental_health_study/mental_health_data.csv')
+    data_control, data_treat = process_csv(path_data)
     data = np.vstack((data_control, data_treat))
-    npl_ = npl_class(data,int(B),int(m), c, T, seed, lx=10, ly=100, prior=1)
+    npl_ = npl_class(data,int(B),int(m), c, T, r, lx=10, ly=100, prior=1)
     t0 = time.time()
     npl_.draw_samples()
     t1 = time.time()
     total = t1-t0
     sample = npl_.sample
     knots_sample = npl_.knots_sample
-    np.savetxt(folder_path+f'bootstrap_samples_bcr22.txt', sample.reshape((int(B),64)))
-    np.savetxt(folder_path+f'bootstrap_samples_knots_bcr22.txt', knots_sample.reshape((int(B),30)))
+    np.savetxt(folder_path+f'bootstrap_samples_bcr_{r}.txt', sample.reshape((int(B),64)))
+    np.savetxt(folder_path+f'bootstrap_samples_knots_bcr_{r}.txt', knots_sample.reshape((int(B),30)))
     return sample, knots_sample
       
 B = 200 
 m = 1
-c = 100
+c = 1
 T = 100
+R = 56
 
 params = np.array([B,m,c,T])
 
 if __name__=='__main__':
-    sample, knots_sample = train_(params)
+    for r in tqdm(range(R)):
+        path_data = f'./mh_study/sim_data_bcr/sim_data_bcr_{r+45}.csv'
+        sample, knots_sample = train_(params, path_data, r+45)
